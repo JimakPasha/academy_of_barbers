a1754ef357d00f4f3848fc54df4fd50c6b20b6ba
JimakPasha <Jimak@tut.by>
2021-01-06 15:52:44 +0300

    added footer sticky header

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,17 @@
 $(function () {
+
+  $('.menu a').on('click', function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 800);
+  });
   
   $('.reviews-slider__items').slick({
     dots: true,
     arrows: false,
     autoplay: true,
-    autoplaySpeed: 2500
+    autoplaySpeed: 3500
   });
 
 });
